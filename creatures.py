aef38ffeff3ab29e1fabba168b51b9dad6532ecd
Sebastian Pietras <seba00767@gmail.com>
2019-11-16 15:02:00 +0100

    Added methods to get cards on given stack

--- creatures.py
+++ creatures.py
@@ -2,14 +2,14 @@ class DeckCreature:
     def __init__(self, genome):
         self._genome = genome
 
-    def get_sum_of_a(self):
-        return 0  # TODO: return sum of cards on deck A
+    def get_stack(self, stack_value):
+        return [index+1 for index, stack in enumerate(self._genome) if stack == stack_value]
 
-    def get_product_of_b(self):
-        return 0  # TODO: return product of cards on deck A
+    def get_stack_len(self, stack_value):
+        return len(self.get_stack(stack_value))
 
     def get_genome(self):
         return self._genome
 
     def get_cards_length(self):
-        return 0  # TODO: return number of cards
+        return len(genome)
