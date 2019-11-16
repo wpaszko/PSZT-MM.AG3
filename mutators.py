cb48285a0138962b0ae90f6cb8b69bb37f9d465c
Sebastian Pietras <seba00767@gmail.com>
2019-11-16 21:27:33 +0100

    Implemented RandomIndependentSwapMutator

--- mutators.py
+++ mutators.py
@@ -10,3 +10,20 @@ class RandomIndependentSwitchMutator:
         new_genome = [not locus if random.random() < self._probability else locus
                       for locus in creature.get_genome()]
         return self._creature_class(new_genome)
+
+
+class RandomIndependentSwapMutator:
+    def __init__(self, creature_class, probability):
+        self._creature_class = creature_class
+        self._probability = probability
+
+    def mutate(self, creature):
+        new_genome = list(creature.get_genome())
+        idx = range(creature.get_cards_length())
+
+        for current_id in idx:
+            if random.random() < self._probability:
+                random_id = random.choice(idx)
+                new_genome[current_id], new_genome[random_id] = new_genome[random_id], new_genome[current_id]
+
+        return self._creature_class(new_genome)
