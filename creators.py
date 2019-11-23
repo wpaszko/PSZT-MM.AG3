17ad16129fbb44b0f444e95a2d7d3cf4058ec918
wpaszko <293114@pw.edu.pl>
2019-11-23 17:05:33 +0100

    Random population generator

--- creators.py
+++ creators.py
@@ -1,6 +1,10 @@
+import random
+
+
 class RandomCreator:
-    def __init__(self, creature_class):
+    def __init__(self, creature_class, n):
         self._creature_class = creature_class
+        self._n = n
 
     def create(self, size):
-        return [self._creature_class([]) for i in range(size)]  # TODO: return population of given size
+        return [self._creature_class([random.choice([True, False]) for i in range(self._n)]) for i in range(size)]
