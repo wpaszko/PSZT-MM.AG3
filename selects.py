81827bcc19c4a8020ff883b1b8220d86bee41b51
wpaszko <293114@pw.edu.pl>
2019-12-02 15:14:18 +0100

    Comments to code added

--- selects.py
+++ selects.py
@@ -21,7 +21,7 @@ class ConsecutiveSelection:
     Consecutive Selection
 
     Most basic selector. Picks consecutive best creatures.
-    It sorts creatures based on fitnesses and picks size of them from left.
+    Sorts creatures based on fitnesses and picks size of them from left.
     """
 
     def select(self, population, fitnesses, size):
@@ -33,7 +33,9 @@ class SwapRandomSelection:
     """
     Swap Random Selection
 
-    Sorts creatures based on fitnesses, randomly swaps some of them and picks size of them from left.
+    Sorts creatures based on fitnesses, then randomly swaps some of them and picks size of them from left.
+
+    Also elitist strategy is used: the very best creature is always picked
     """
 
     def __init__(self, probability, rng=random.Random()):
@@ -45,13 +47,12 @@ class SwapRandomSelection:
         return [x for _, x in sorted(self._swap(best, size), key=lambda x: x[0])]
 
     def _swap(self, best, size):
-        half_index = int(len(best) / 2)
-        left_half = best[:half_index]
-        right_half = best[half_index:]
-        for current_id, x in enumerate(left_half):
-            if self._rng.random() < self._probability:
-                id = self._rng.randint(half_index, len(best) - 1)
-                best[current_id], best[id] = best[id], best[current_id]
+        half_index = int(len(best) / 2)  # divide temporary population into better and worse creatures
+        left_half = best[1:half_index]  # best[0] is the very best, therefore cannot be swapped
+        for current_id, x in enumerate(left_half):  # going by the better half of temporary population
+            if self._rng.random() < self._probability:  # if is about to swap
+                id = self._rng.randint(half_index, len(best) - 1)   # picks random creature from the worse half
+                best[current_id], best[id] = best[id], best[current_id]  # and swaps places with them
         return best[:size]
 
 
@@ -64,7 +65,7 @@ class TanhSelection:
         - best fitness is given value close to 1
         - mean fitness is given value equal to 0.5
 
-    In result creatures with best fitnesses are almost always picked, with worst fitnesses are almost never picked
+    In result creatures with the best fitnesses are almost always picked, with the worst fitnesses are almost never picked
     and with fitnesses close to mean are chosen the most randomly
 
     Also elitist strategy is used: the very best creature is always picked
@@ -80,7 +81,7 @@ class TanhSelection:
         ln = np.log(fitness_mean)
         fitnesses = [i for i, _ in best]
         p = [0.5 * (1 - np.tanh(((2 * ln * i) / fitness_mean) - ln)) for i in fitnesses]
-
+        
         p[0] = 0.0
 
         p /= sum(p)
@@ -98,8 +99,8 @@ class ExponentialSelection:
     Picks creature based on probability.
     Probability is proportional to exponential function normalized by mean and standard deviation
 
-    In result creatures with best lowest fitnesses are picked very often and probability shrinks exponentially for
-    higher fitnesses
+    In result creatures with the best (the lowest) fitnesses are picked very often and probability shrinks exponentially
+    for higher fitnesses
 
     Also elitist strategy is used: the very best creature is always picked
 
@@ -108,26 +109,27 @@ class ExponentialSelection:
     """
 
     def __init__(self, selective_pressure, np_rng=np.random.RandomState()):
-        self._selective_pressure = selective_pressure
+        self._selective_pressure = selective_pressure  # around 1.0
         self._np_rng = np_rng
 
     def select(self, population, fitnesses, size):
         best = [(y, x) for y, x in sorted(zip(fitnesses, population), key=lambda x: x[0])]
 
-        fitness_std = np.std(fitnesses)
+        fitness_std = np.std(fitnesses)  # standard deviation
         fitness_mean = np.mean(fitnesses)
         fitnesses = [i for i, _ in best]
 
-        if fitness_std > EPSILON:
+        # p = list of probabilities
+        if fitness_std > EPSILON:  # standard deviation is denominator of the fraction
             p = [np.exp(-self._selective_pressure * (i - fitness_mean) / fitness_std) for i in fitnesses]
-        else:
+        else:  # if 0 all the probabilities are equal
             p = np.full(len(fitnesses), 1.0)
 
-        p[0] = 0.0
+        p[0] = 0.0  # elitist strategy, the very best doesn't take part in a draw
 
-        p /= sum(p)
+        p /= sum(p)  # all the probabilities have to sum up to 1, therefore we divide all of them by their sum
 
-        best_selected = [best[0]]
+        best_selected = [best[0]]  # elitist strategy, the very best is always chosen
         for i in self._np_rng.choice(len(best), size - 1, replace=False, p=p):
             best_selected.append(best[i])
         return [x for _, x in sorted(best_selected, key=lambda x: x[0])]
